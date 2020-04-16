a746c627febe8d7a9fe20c94a08cfc8ca977a444
rolandviper <56328676+rolandviper@users.noreply.github.com>
2020-04-16 15:35:26 +0800

    initial add of lower3rd

---
+++ dashboard/panel.js
@@ -0,0 +1,11 @@
+function playl3() {
+	nodecg.sendMessage('playl3');
+}
+
+function playCaster() {
+	nodecg.sendMessage('playcaster');
+}
+
+function playComing() {
+	nodecg.sendMessage('playcoming');
+}
