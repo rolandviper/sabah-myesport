5ac89c57a8451eb34e313aa92effe5818a58e6bf
rolandviper <56328676+rolandviper@users.noreply.github.com>
2020-04-18 16:27:34 +0800

    added hooks for MLBB draft and inGame

--- dashboard/panel.js
+++ dashboard/panel.js
@@ -9,3 +9,31 @@ function playCaster() {
 function playComing() {
 	nodecg.sendMessage('playcoming');
 }
+
+function playMLBBdraft() {
+	nodecg.sendMessage('playDraft');
+}
+
+function stopMLBBdraft() {
+	nodecg.sendMessage('stopDraft');
+}
+
+function playMLBBgame() {
+	nodecg.sendMessage('playMLBB');
+}
+
+function stopMLBBgame() {
+	nodecg.sendMessage('stopMLBB');
+}
+
+function playMLBBpart() {
+	nodecg.sendMessage('playPart');
+}
+
+function playMLBB32() {
+	nodecg.sendMessage('play32');
+}
+
+function playPlayoff() {
+	nodecg.sendMessage('playPlayoff');
+}
