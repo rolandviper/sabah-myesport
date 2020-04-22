f1023ae4783b30061becbe16ab35f82c4b8fb621
rolandviper <56328676+rolandviper@users.noreply.github.com>
2020-04-22 22:20:55 +0800

    updated panels and pubgingamehooks

--- dashboard/panel.js
+++ dashboard/panel.js
@@ -41,3 +41,75 @@ function playpubg() {
 function stoppubg() {
 	nodecg.sendMessage('stopppubg');
 }
+
+function playgrA() {
+	nodecg.sendMessage('playgrA');
+}
+
+function stopgrA() {
+	nodecg.sendMessage('stopgrA');
+}
+
+function playgrB() {
+	nodecg.sendMessage('playgrB');
+}
+
+function stopgrB() {
+	nodecg.sendMessage('stopgrB');
+}
+
+function playgrC() {
+	nodecg.sendMessage('playgrC');
+}
+
+function stopgrC() {
+	nodecg.sendMessage('stopgrC');
+}
+
+function playgrD() {
+	nodecg.sendMessage('playgrD');
+}
+
+function stopgrD() {
+	nodecg.sendMessage('stopgrD');
+}
+
+function playgrE() {
+	nodecg.sendMessage('playgrE');
+}
+
+function stopgrE() {
+	nodecg.sendMessage('stopgrE');
+}
+
+function playgrF() {
+	nodecg.sendMessage('playgrF');
+}
+
+function stopgrF() {
+	nodecg.sendMessage('stopgrF');
+}
+
+function playgrG() {
+	nodecg.sendMessage('playgrG');
+}
+
+function stopgrG() {
+	nodecg.sendMessage('stopgrG');
+}
+
+function playgrH() {
+	nodecg.sendMessage('playgrH');
+}
+
+function stopgrA() {
+	nodecg.sendMessage('stopgrH');
+}
+
+function playFinal() {
+	nodecg.sendMessage('playfinal');
+}
+
+function stopFinal() {
+	nodecg.sendMessage('stopfinal');
+}
